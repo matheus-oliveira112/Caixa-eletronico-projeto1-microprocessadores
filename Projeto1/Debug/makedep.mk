ed2088b35d631c942349d0c5c44f1bdf79b2cd35
matheus-oliveira112 <matheus.veira212@gmail.com>
2026-04-05 19:38:41 -0300

    Add files via upload

---
+++ Projeto1/Debug/makedep.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+atrasos.c
+
+comunicacao.c
+
+DataHora.c
+
+DISPLAY.c
+
+main.c
+
+operacoes.c
+
+SERIAL.c
+
+teclado.c
+
---
+++ Projeto1/comunicacao.c
@@ -0,0 +1,67 @@
+/*
+ * comunicacao.c
+ *
+ * Created: 27/05/2025 01:04:28
+ *  Author: mathe
+ */ 
+
+#include <avr/io.h>
+
+void comunica_saque() {
+	UDR0 = 'C';
+	while(!(UCSR0A & (1<<TXC0)));
+	UDR0 = 'S';
+	while(!(UCSR0A & (1<<TXC0)));
+	UDR0 = 6;								//Numero de byts do valor do saque
+	while(!(UCSR0A & (1<<TXC0)));
+}
+
+
+void comunica_saldo() {
+	UDR0 = 'C';
+	while(!(UCSR0A & (1<<TXC0)));
+	UDR0 = 'V';
+	while(!(UCSR0A & (1<<TXC0)));
+}
+
+void comunica_fechamento() {
+	UDR0 = 'C';
+	while(!(UCSR0A & (1<<TXC0)));
+	UDR0 = 'F';
+	while(!(UCSR0A & (1<<TXC0)));
+}
+
+void comunica_liberacao() {
+	UDR0 = 'C';
+	while(!(UCSR0A & (1<<TXC0)));
+	UDR0 = 'L';
+	while(!(UCSR0A & (1<<TXC0)));
+}
+
+void comunica_travamento() {
+	UDR0 = 'C';
+	while(!(UCSR0A & (1<<TXC0)));
+	UDR0 = 'T';
+	while(!(UCSR0A & (1<<TXC0)));
+}
+
+void comunica_horario() {
+	UDR0 = 'C';
+	while(!(UCSR0A & (1<<TXC0)));
+	UDR0 = 'H';
+	while(!(UCSR0A & (1<<TXC0)));
+}
+
+void comunica_entrada() {
+	UDR0 = 'C';
+	while(!(UCSR0A & (1<<TXC0)));
+	UDR0 = 'E';
+	while(!(UCSR0A & (1<<TXC0)));
+}
+
+void comunica_operando() {
+	UDR0 = 'C';
+	while(!(UCSR0A & (1<<TXC0)));
+	UDR0 = 'O';
+	while(!(UCSR0A & (1<<TXC0)));
+}
